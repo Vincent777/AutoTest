2fabadc0c1392648442eab3745affa958c63b8b4
limingge <limingge@xcoresigma.com>
2026-08-05 10:57:40 +0800

    update

--- ascend_test_suite/siginfer_ascend_test.sh
+++ ascend_test_suite/siginfer_ascend_test.sh
@@ -542,15 +542,17 @@ for option in "${schedule_policies[@]}"; do
                     
                     # 开始执行测试
                     if [ $TEST_PARAM == "Random" ]; then
-                        multiplier=4
-                        concurrency_list=(1 5 10 20 50 100 150 200)
+                        # multiplier=4
+                        # concurrency_list=(1 5 10 20 50 100 150 200)
+                        multiplier=1
+                        concurrency_list=(1)
                         length_pairs=(
                             "128:128"
-                            "128:1024"
-                            "128:2048"
-                            "1024:1024"
-                            "2048:2048"
-                            # "4096:1024"
+                            # "128:1024"
+                            # "128:2048"
+                            # "1024:1024"
+                            # "2048:2048"
+                            # # "4096:1024"
                             # "1024:4096"
                             # "30000:2048"
                             # "126000:2048"
