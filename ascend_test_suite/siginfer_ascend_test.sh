baba77de9c10dd1ef5edb1bea3b63842b4f3ef90
limingge <limingge@xcoresigma.com>
2026-09-02 12:38:14 +0800

    updates

--- ascend_test_suite/siginfer_ascend_test.sh
+++ ascend_test_suite/siginfer_ascend_test.sh
@@ -800,10 +800,8 @@ for option in "${schedule_policies[@]}"; do
 
                     # 开始执行测试
                     if [ $TEST_PARAM == "Random" ]; then
-                        # multiplier=4
-                        # concurrency_list=(1 5 10 20 50 100 150 200)
-                        multiplier=1
-                        concurrency_list=(1)
+                        multiplier=4
+                        concurrency_list=(1 5 10 20 50 100 150 200)
                         length_pairs=(
                             "128:128"
                             "128:1024"
--- tianshu_test_suite/README.md
+++
@@ -1,9 +0,0 @@
-## 使用方法 ##
-进入tianshu_test_suite目录，执行:
-    bash tianshu_resource_monitor.sh s_yangshuo Smoke main-98e050eb
-    bash tianshu_resource_monitor.sh s_yangshuo Performance <Random|SharedGPT> main-98e050eb
-
-参数1: 服务器上的用户名
-参数2: 测试类型，分为Smoke、Performance、Stability、Accuracy
-参数3：测试参数，比如Performance测试类型对应的Random或SharedGPT
-参数2：测试版本（镜像tag），如果为空，则默认为Latest
